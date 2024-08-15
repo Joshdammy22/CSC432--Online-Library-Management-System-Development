d2b420bded6a0e46fdb2e18a78d3703bd3b7f069
Damilola Oladoke <122222424+Joshdammy22@users.noreply.github.com>
2024-08-15 01:25:42 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Reader(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
     reference_id = models.CharField(max_length=50, unique=True)
